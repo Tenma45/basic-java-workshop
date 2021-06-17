513bbcd164ee2eac021d063c7039d1f864efe71c
Worapat Tubtimdee <worapat.t@kbtg.tech>
2021-06-17 15:03:24 +0700

    Format style

--- demo01/src/MyRange.java
+++ demo01/src/MyRange.java
@@ -2,18 +2,23 @@ public class MyRange {
     public static void main(String[] args) {
         // check args format
         if(args[0].split(",").length!=2){throw new Error("Invalid format");}
+
         // partition with ","
         String input1 = args[0].split(",")[0];
         String input2 = args[0].split(",")[1];
+
         // keep first and last character as sign
         Character sign1 = input1.charAt(0);
         Character sign2 = input2.charAt(input2.length()-1);
+
         // keep first and second number
         String number1 = input1.substring(1);
         String number2 = input2.substring(0, input2.length() - 1);
+
         // parse to integer
         int start = Integer.parseInt(number1);
         int stop= Integer.parseInt(number2);
+
         // sign condition
         if(new Character('(').equals(sign1)){
             start+=1;
@@ -21,14 +26,15 @@ public class MyRange {
         else if(new Character('[').equals(sign1)){
             ;
         }
-        else {throw new Error("invalid format");}
+        else {throw new Error("Invalid format");}
         if(new Character(')').equals(sign2)){
             stop-=1;
         }
         else if(new Character(']').equals(sign2)){
             ;
         }
-        else {throw new Error("invalid format");}
+        else {throw new Error("Invalid format");}
+
         // print out
         for (int i=start;i<=stop;i++){
             System.out.print(i);
