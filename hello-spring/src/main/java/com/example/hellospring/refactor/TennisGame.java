93168b628247432aabd769b3a147e9e66c9a6711
Worapat Tubtimdee <worapat.t@kbtg.tech>
2021-06-18 17:56:25 +0700

    [refactor] naming

--- hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame.java
+++ hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame.java
@@ -2,35 +2,35 @@ package com.example.hellospring.refactor;
 
 public class TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int player1Score;
+    private int player2Score;
+    private String player1Name;
+    private String player2Name;
 
-    public TennisGame(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String calledScore;
+        if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
+            String[] calledScoreSet = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            calledScore = calledScoreSet[player1Score];
+            return (player1Score == player2Score) ? calledScore + "-All" : calledScore + "-" + calledScoreSet[player2Score];
         } else {
-            if (p1 == p2)
+            if (player1Score == player2Score)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            calledScore = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + calledScore : "Win for " + calledScore;
         }
     }
 
     public void wonPoint(String playerName) {
         if ("player1".equals(playerName))
-            this.p1 += 1;
+            this.player1Score += 1;
         else
-            this.p2 += 1;
+            this.player2Score += 1;
 
     }
 
