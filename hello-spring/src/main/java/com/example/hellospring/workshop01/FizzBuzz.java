f4075af521198e19c40d6667fb99af725613ef45
Worapat Tubtimdee <worapat.t@kbtg.tech>
2021-06-18 16:37:29 +0700

    FizzBuzz with condition interface

--- hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
@@ -1,4 +1,28 @@
 package com.example.hellospring.workshop01;
 
 public class FizzBuzz {
+
+    private int input;
+
+    public FizzBuzz(int input){
+        this.input = input;
+    }
+
+    public String getResult() {
+        // Prepare all conditions
+        MyCondition[] conditions = new MyCondition[]{
+                new FizzBuzzCondition(),
+                new FizzCondition(),
+                new BuzzCondition(),
+                new KBTGCondition()
+        };
+
+        for (MyCondition condition : conditions) {
+            if(condition.check(this.input)) {
+                return condition.print();
+            }
+        }
+
+        return Integer.toString(this.input);
+    }
 }
