3af2a3ec57a25af758407eca2840a44b6ece4103
Worapat Tubtimdee <worapat.t@kbtg.tech>
2021-06-21 12:14:32 +0700

    employee api

---
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.hellorest.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Id;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employee/{id}")
+    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+        // Validate id => Number only
+
+        int _id;
+        try {
+            _id = Integer.parseInt(id);
+        }
+        catch (NumberFormatException e){
+            _id = 0;
+        }
+        return new EmployeeResponse(_id, "Somkiat", "Pui");
+    }
+
+}
